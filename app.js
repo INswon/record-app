4182ada1679f5cab7cf99fe5495b5d12947d9c5f
INswon <152695318+INswon@users.noreply.github.com>
2024-08-05 13:23:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+//querySelector()で最初の一つの要素を取得する
 const btn = document.querySelector('.submitBtn')
 btn.addEventListener('click', () => {
+    //class(inputTitle)データを取得
     const input = document.querySelector('.inputTitle').value
+    //取得された映画タイトルを引数としてcreatLi(リストアイテムとして生成し、既存のリストに追加する処理を実現)している
     createLi(input)
+    // 入力フィールドをクリア
     document.querySelector('.inputTitle').value = ''
 })
 
